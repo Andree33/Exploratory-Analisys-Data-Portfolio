c218477051a3721f830b25c24c89d14824c6c64e
Andre Grilo <andre.jacinto.grilo@tecnico.ulisboa.pt>
2026-02-02 22:28:02 +0000

    a

--- temporal_analysis.py
+++ temporal_analysis.py
@@ -1,3 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot
 import numpy as np
-print(f"Versão do Pandas: {np.__version__}")
+data = 'BTC-market-analysis_2021-25.csv'
+df = pd.read_csv(data)
+df = df[["date", "btc_return", "sp500_return", "gold_return"]]
+
+df = df.replace(0.000000, pd.NA)
+df = df.dropna(subset=["sp500_return"])        
+df = df.dropna(subset=["gold_return"])  
+
+
+
+print(df.to_string())
